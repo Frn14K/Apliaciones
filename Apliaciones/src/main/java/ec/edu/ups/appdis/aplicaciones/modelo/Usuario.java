37d215eedba66d8c3d655158cacef7b53aaf74eb
Frn14K <fvillavicenciob@est.ups.edu.ec>
2017-11-21 11:33:22 -0500

    comit cedula

--- Apliaciones/src/main/java/ec/edu/ups/appdis/aplicaciones/modelo/Usuario.java
+++ Apliaciones/src/main/java/ec/edu/ups/appdis/aplicaciones/modelo/Usuario.java
@@ -11,5 +11,14 @@ public class Usuario
 	@Id
 	@Column (name = "per_cedula",nullable = false) 
 	private String cedula;
+
+	public String getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+	
 	
 }
